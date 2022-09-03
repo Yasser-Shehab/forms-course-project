cfd5fdcd54e69aa9f7843e1431817e6dce3e8f53
AquaApple <yassershehab9@gmail.com>
2022-09-03 21:56:37 +0200

    Added input-hanlder & submit-handler to SimpleInputForm

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,21 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
