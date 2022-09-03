2aa5b1c676a43a0defbd8dcf8434a6ab152a70ed
AquaApple <yassershehab9@gmail.com>
2022-09-04 01:11:50 +0200

    Added Name Input Hook to Simple Input component

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import { validateEmail } from "../util/regularExpression";
+import useInput from "../hooks/use-input";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const {
+    value: enteredName,
+    hasError: nameInputHasError,
+    isValid: enteredNameIsValid,
+    reset: resetName,
+    valueChangeHanlder: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+  } = useInput((value) => value.trim() !== "");
+
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
-  const enteredNameIsValid = enteredName.trim() !== "";
-  const nameInputInvalid = !enteredNameIsValid && enteredNameTouched;
   const enteredEmailIsValid = validateEmail(enteredEmail);
   const emailInputInvalid = !enteredEmailIsValid && enteredEmailTouched;
   let formIsValid = false;
@@ -24,37 +30,34 @@ const SimpleInput = (props) => {
   const emailInputHandler = (e) => {
     setEnteredEmail(e.target.value);
   };
-  const nameInputBlurHandler = (e) => {
-    setEnteredNameTouched(true);
-  };
-
-  const nameInputHandler = (e) => {
-    setEnteredName(e.target.value);
-  };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
 
-    setEnteredNameTouched(true);
-
-    if (!nameInputInvalid) {
+    if (!nameInputHasError) {
       return;
     }
 
     console.log(enteredName);
-    setEnteredNameTouched(false);
+    resetName();
     setEnteredEmailTouched(false);
   };
 
-  const nameInputClasses = nameInputInvalid ? "form-control invalid" : "form-control ";
+  const nameInputClasses = nameInputHasError ? "form-control invalid" : "form-control ";
   const emailInputClasses = emailInputInvalid ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputHandler} onBlur={nameInputBlurHandler} />
-        {nameInputInvalid && <p className="error-text">Name must not be empty</p>}
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName}
+        />
+        {nameInputHasError && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className={emailInputClasses}>
         <label htmlFor="email">Email</label>
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -6,6 +6,28 @@ const useInput = (validateValue) => {
 
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHanlder = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    reset,
+    valueChangeHanlder,
+    inputBlurHandler,
+  };
 };
 
 export default useInput;
